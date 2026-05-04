c9071e67527046b58660d18e7c250dac01a8f4ae
Durva Kadam <durvakadam204@gmail.com>
2026-05-04 16:58:03 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
-users = [("Alice", 25), ("Bob", 15)]
 
-for name, age in users:
-    activity = "driving" if age >= 18 else "school"
-    print(f"{name} did {activity}")
+
+def max_val(a, b):
+    if a > b:
+        return a
+    return b
