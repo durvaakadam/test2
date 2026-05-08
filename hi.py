51c26d08ea79d9a14f839bc7f0d88ad873983338
Durva Kadam <durvakadam204@gmail.com>
2026-05-08 20:50:22 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -3,7 +3,39 @@ class Demo:
         source = "too long"
         dest = [""] * 5
         trigger = 5
+class Calculator:
 
+    def add(self, a, b):
+        result = a + b
+        return result
+
+    def subtract(self, a, b):
+        result = a - b
+        return result
+
+
+class Message:
+
+    def display(self, text):
+        print(text)
+
+
+def main():
+
+    calc = Calculator()
+    msg = Message()
+
+    sum_result = calc.add(10, 5)
+    sub_result = calc.subtract(10, 5)
+
+    if sum_result > sub_result:
+        msg.display("Addition result is greater")
+    else:
+        msg.display("Subtraction result is greater")
+
+
+if __name__ == "__main__":
+    main()
         if trigger == 5:
             for i in range(len(source)):
                 dest[i] = source[i]
