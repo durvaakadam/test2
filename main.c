1948e1ea4026d537ff51a3739b2594e457e60a59
Durva Kadam <durvakadam204@gmail.com>
2026-05-08 12:13:15 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+int main(int argc, char **argv) {
+    char source[] = "too long";
+    char dest[5];
+    int trigger = 5;
+    if (trigger == 5) {
+        strcpy(dest, source);
+    }
+
+    return 0;
+}
